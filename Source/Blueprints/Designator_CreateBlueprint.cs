cbf31a9b87c5330768b7f501befcc26aa7bd2f0e
FluffierThanThou <fluffy.l2032@gmail.com>
2017-05-05 23:25:46 +0200

    initial A17 version

--- Source/Blueprints/Designator_CreateBlueprint.cs
+++ Source/Blueprints/Designator_CreateBlueprint.cs
@@ -17,14 +17,14 @@ namespace Blueprints
         public Designator_CreateBlueprint()
         {
             // Silly A13+ workaround
-            LongEventHandler.ExecuteWhenFinished( delegate
-            {
-                Resources.Icon_AddBlueprint = ContentFinder<Texture2D>.Get( "Icons/AddBlueprint", true );
-                Resources.Icon_Blueprint = ContentFinder<Texture2D>.Get( "Icons/Blueprint", true );
-                Resources.Icon_Edit = ContentFinder<Texture2D>.Get( "Icons/Edit", true );
+            LongEventHandler.ExecuteWhenFinished(delegate
+           {
+               Resources.Icon_AddBlueprint = ContentFinder<Texture2D>.Get("Icons/AddBlueprint", true);
+               Resources.Icon_Blueprint = ContentFinder<Texture2D>.Get("Icons/Blueprint", true);
+               Resources.Icon_Edit = ContentFinder<Texture2D>.Get("Icons/Edit", true);
 
-                icon = Resources.Icon_AddBlueprint;
-            } );
+               icon = Resources.Icon_AddBlueprint;
+           });
 
             defaultLabel = "Fluffy.Blueprints.Create".Translate();
             defaultDesc = "Fluffy.Blueprints.CreateHelp".Translate();
@@ -47,96 +47,96 @@ namespace Blueprints
 
         #region Public Methods
 
-        public override AcceptanceReport CanDesignateCell( IntVec3 loc )
+        public override AcceptanceReport CanDesignateCell(IntVec3 loc)
         {
-            var things = loc.GetThingList( Map );
-            return loc.InBounds( Map ) &&
-                ( loc.GetTerrain( Map ).IsValidBlueprintTerrain() ||
-                ( !things.NullOrEmpty() &&
-                   things.Any( thing => thing.IsValidBlueprintThing() ) ) );
+            var things = loc.GetThingList(Map);
+            return loc.InBounds(Map) &&
+                (loc.GetTerrain(Map).IsValidBlueprintTerrain() ||
+                (!things.NullOrEmpty() &&
+                   things.Any(thing => thing.IsValidBlueprintThing())));
         }
 
-        public override void ProcessInput( Event ev )
+        public override void ProcessInput(Event ev)
         {
-            if ( ev.button == 1 )
+            if (ev.button == 1)
             {
                 List<FloatMenuOption> options = new List<FloatMenuOption>();
 
-                foreach ( var file in Controller.GetSavedFilesList() )
+                foreach (var file in Controller.GetSavedFilesList())
                 {
-                    string name = Path.GetFileNameWithoutExtension( file.FileInfo.Name );
-                    if ( Controller.FindBlueprint( name ) == null )
+                    string name = Path.GetFileNameWithoutExtension(file.Name);
+                    if (Controller.FindBlueprint(name) == null)
                     {
-                        options.Add( new FloatMenuOption( "Fluffy.Blueprints.LoadFromXML".Translate( name ), delegate
-                        { Controller.Add( Controller.LoadFromXML( file ) ); } ) );
+                        options.Add(new FloatMenuOption("Fluffy.Blueprints.LoadFromXML".Translate(name), delegate
+                    { Controller.Add(Controller.LoadFromXML(file.Name)); }));
                     }
                 }
 
-                if ( options.NullOrEmpty() )
-                    Messages.Message( "Fluffy.Blueprints.NoStoredBlueprints".Translate(), MessageSound.RejectInput );
+                if (options.NullOrEmpty())
+                    Messages.Message("Fluffy.Blueprints.NoStoredBlueprints".Translate(), MessageSound.RejectInput);
                 else
-                    Find.WindowStack.Add( new FloatMenu( options ) );
+                    Find.WindowStack.Add(new FloatMenu(options));
                 return;
             }
 
-            base.ProcessInput( ev );
+            base.ProcessInput(ev);
         }
 
-        public override void DesignateMultiCell( IEnumerable<IntVec3> cells )
+        public override void DesignateMultiCell(IEnumerable<IntVec3> cells)
         {
             // bail out if empty
-            if ( cells == null || cells.Count() == 0 )
+            if (cells == null || cells.Count() == 0)
             {
-                Messages.Message( "Fluffy.Blueprints.CannotCreateBluePrint_NothingSelected".Translate(), MessageSound.RejectInput );
+                Messages.Message("Fluffy.Blueprints.CannotCreateBluePrint_NothingSelected".Translate(), MessageSound.RejectInput);
                 return;
             }
 
             // get list of buildings in the cells, note that this includes frames and blueprints, and so _may include floors!_
-            List<Thing> things = new List<Thing>( cells.SelectMany( cell => cell.GetThingList( Map )
-                                                       .Where( thing => thing.IsValidBlueprintThing() ) )
-                                                       .Distinct() );
+            List<Thing> things = new List<Thing>(cells.SelectMany(cell => cell.GetThingList(Map)
+                                                     .Where(thing => thing.IsValidBlueprintThing()))
+                                                       .Distinct());
 
             // get list of creatable terrains
             List<Pair<TerrainDef, IntVec3>> terrains = new List<Pair<TerrainDef, IntVec3>>();
-            terrains.AddRange( cells.Select( cell => new Pair<TerrainDef, IntVec3>( cell.GetTerrain( Map ), cell ) )
-                                    .Where( p => p.First.IsValidBlueprintTerrain() ) );
+            terrains.AddRange(cells.Select(cell => new Pair<TerrainDef, IntVec3>(cell.GetTerrain(Map), cell))
+                                    .Where(p => p.First.IsValidBlueprintTerrain()));
 
             // get edges of blueprint area
             // (might be bigger than selected region, but never smaller).
-            var allCells = cells.Concat( things.SelectMany( thing => thing.OccupiedRect().Cells ) );
+            var allCells = cells.Concat(things.SelectMany(thing => thing.OccupiedRect().Cells));
 
-            int left = allCells.Min( cell => cell.x );
-            int top = allCells.Max( cell => cell.z );
-            int right = allCells.Max( cell => cell.x );
-            int bottom = allCells.Min( cell => cell.z );
+            int left = allCells.Min(cell => cell.x);
+            int top = allCells.Max(cell => cell.z);
+            int right = allCells.Max(cell => cell.x);
+            int bottom = allCells.Min(cell => cell.z);
 
             // total size ( +1 because x = 2 ... x = 4 => 4 - 2 + 1 cells )
-            IntVec2 size = new IntVec2( right - left + 1, top - bottom + 1 );
+            IntVec2 size = new IntVec2(right - left + 1, top - bottom + 1);
 
             // fetch origin for default (North) orientation
-            IntVec3 origin = Resources.CenterPosition( new IntVec3( left, 0, bottom ), size, Rot4.North );
+            IntVec3 origin = Resources.CenterPosition(new IntVec3(left, 0, bottom), size, Rot4.North);
 
             // create list of buildables
             List<BuildableInfo> buildables = new List<BuildableInfo>();
-            foreach ( var thing in things )
+            foreach (var thing in things)
             {
-                buildables.Add( new BuildableInfo( thing, origin ) );
+                buildables.Add(new BuildableInfo(thing, origin));
             }
-            foreach ( var terrain in terrains )
+            foreach (var terrain in terrains)
             {
-                buildables.Add( new BuildableInfo( terrain.First, terrain.Second, origin ) );
+                buildables.Add(new BuildableInfo(terrain.First, terrain.Second, origin));
             }
 
             // try to get a decent default name: check if selection contains only a single room - if so, that's a decent name.
-            Room room = origin.GetRoom( Map );
+            Room room = origin.GetRoom(Map);
             string defaultName = null;
-            if ( room != null && room.Role != RoomRoleDefOf.None )
+            if (room != null && room.Role != RoomRoleDefOf.None)
                 defaultName = room.Role.LabelCap;
             // TODO: multiple (same) rooms, etc.
 
             // add to controller - controller handles adding to designations
-            Blueprint blueprint = new Blueprint( buildables, size, defaultName );
-            Controller.Add( blueprint );
+            Blueprint blueprint = new Blueprint(buildables, size, defaultName);
+            Controller.Add(blueprint);
 
 #if DEBUG
             blueprint.Debug();
