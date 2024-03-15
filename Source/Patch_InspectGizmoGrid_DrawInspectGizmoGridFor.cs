d2d0213dfb4d37155db837f72f4c7b39f132b538
FluffierThanThou <fluffy.l2032@gmail.com>
2024-03-15 20:13:38 +0100

    fix copy gizmo injection patch

--- Source/Patch_InspectGizmoGrid_DrawInspectGizmoGridFor.cs
+++ Source/Patch_InspectGizmoGrid_DrawInspectGizmoGridFor.cs
@@ -1,4 +1,4 @@
-﻿// Copyright Karel Kroeze, 2021-2021.
+// Copyright Karel Kroeze, 2021-2021.
 // Blueprints/Blueprints/Patch_InspectGizmoGrid_DrawInspectGizmoGridFor.cs
 
 using System.Collections.Generic;
@@ -9,16 +9,13 @@ using HarmonyLib;
 using RimWorld;
 using Verse;
 
-namespace Blueprints
-{
+namespace Blueprints {
     [HarmonyPatch(typeof(InspectGizmoGrid), nameof(InspectGizmoGrid.DrawInspectGizmoGridFor))]
-    public class Patch_InspectGizmoGrid_DrawInspectGizmoGridFor
-    {
-        public static MethodInfo addInfo = AccessTools.Method(typeof(List<Gizmo>), nameof(List<Gizmo>.Add));
+    public class Patch_InspectGizmoGrid_DrawInspectGizmoGridFor {
+        public static MethodInfo listAdd = AccessTools.Method(typeof(List<Gizmo>), nameof(List<Gizmo>.Add));
 
-        public static MethodInfo clearInfo = AccessTools.Method(typeof(List<object>), "Clear");
+        public static MethodInfo clearInfo = AccessTools.Method(typeof(List<Gizmo>), nameof(List<Gizmo>.Clear));
         public static FieldInfo  gizmoList = AccessTools.Field(typeof(InspectGizmoGrid), "gizmoList");
-        public static FieldInfo  objList   = AccessTools.Field(typeof(InspectGizmoGrid), "objList");
 
         public static MethodInfo blueprintGetter = AccessTools
                                                   .Property(typeof(Patch_InspectGizmoGrid_DrawInspectGizmoGridFor),
@@ -29,23 +26,19 @@ namespace Blueprints
         public static Command_CreateBlueprintCopyFromSelected BlueprintCopy =>
             new Command_CreateBlueprintCopyFromSelected();
 
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> _instructions)
-        {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> _instructions) {
             List<CodeInstruction> instructions = _instructions.ToList();
 
 
-            for (int i = 0; i < instructions.Count; i++)
-            {
+            for (int i = 0; i < instructions.Count; i++) {
                 yield return instructions[i];
 
-                if (i > 0 && instructions[i - 1].LoadsField(objList)
-                          && instructions[i].Calls(clearInfo)
-                          && instructions[i + 1].LoadsField(gizmoList))
-                {
+                if (i > 0 && instructions[i - 1].LoadsField(gizmoList)
+                          && instructions[i].Calls(clearInfo)) {
                     Debug.Message("injecting blueprint gizmo");
                     yield return new CodeInstruction(OpCodes.Ldsfld, gizmoList);
                     yield return new CodeInstruction(OpCodes.Call, blueprintGetter);
-                    yield return new CodeInstruction(OpCodes.Callvirt, addInfo);
+                    yield return new CodeInstruction(OpCodes.Callvirt, listAdd);
                 }
             }
         }
